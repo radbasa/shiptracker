ea623d30900b07c5c659d47f6b783143f45781c0
Rodrigo Basa <rad.basa@sesat-tech.com>
2021-09-30 14:15:10 +0800

    workable vertical layout.

--- ShipTracker/ui.R
+++ ShipTracker/ui.R
@@ -1,42 +1,14 @@
-grid_charts <- grid_template(
-    default = list(areas = rbind(c("chart1", "chart2")),
-                   rows_height = c("100%"),
-                   cols_width = c("50%", "50%"))
-)
-
-layout_template = grid_template(
-    default = list(
-        areas = rbind(
-            c("userinput"),
-            c("map"),
-            c("shipinfo")
-        ),
-        cols_width = c("auto"),
-        rows_height = c("120px", "auto", "auto")
-    ),
-    mobile = list(
-        areas = rbind(
-            "userinput",
-            "map",
-            "shipinfo"
-        ),
-        cols_width = c("100%"),
-        rows_height = c("120px", "auto", "400px")
-    )
-)
-
 semanticPage(
-    grid(
-        layout_template,
-        userinput = inputUI("dropdown_inputs"),
-        map = mapOutput("map_output"),
-        shipinfo = segment(
-                tabset(
-                    tabs = list(
-                        list(menu = "Ship Info", content = infoUI("info_card")),
-                        list(menu = "Distance Travelled Histogram", content = chartUI("chart_card"))
-                    )
+    vertical_layout(
+        inputUI("dropdown_inputs"),
+        mapOutput("map_output"),
+        segment(
+            tabset(
+                tabs = list(
+                    list(menu = "Ship Info", content = infoUI("info_card")),
+                    list(menu = "Distance Travelled Histogram", content = chartUI("chart_card"))
                 )
             )
+        )
     )
 )
